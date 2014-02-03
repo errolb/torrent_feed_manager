50ea1938bdf5d7035716362df172a0b0987b6721
errolb <errolburger1@gmail.com>
2014-02-03 16:29:15 +0200

    Smart redirect via  and removed stray var_dumps etc

--- read.php
+++ read.php
@@ -1,9 +1,10 @@
 <?php
 		# Set $path_to_json to where you create the torrent scraper's json db file is
-		# Be sure to create this file in a convenient location.
+		# Be sure to create this file in a convenient location for torrent-feed-scraper.
+		# View and edit the source code as necessary.
+
     $path_to_json = '/Users/admin/Desktop/feed_list.json';
     $command = 'cat ' . $path_to_json;
     $rawjson = shell_exec($command);
     $feed_list = json_decode($rawjson);
-
 ?>
